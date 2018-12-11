8bb80ae45edbd75819a08b3f4db0edcd5642504f
轻剑快马 <xiaoranran1993@outlook.com>
2018-12-11 20:07:56 +0800

    test04

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <h1>hello world</h1>
     <h2>Bravo!</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita facere consequuntur harum, nam dolore sed rem quasi minus eligendi distinctio unde, ducimus velit dolorum nobis sapiente ipsam voluptates esse quam.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla et praesentium laudantium quas aspernatur rerum tempora accusamus maxime eos, consequuntur provident excepturi quos beatae dolorum itaque labore dolor corporis.</p>
   </div>
 </template>
 
